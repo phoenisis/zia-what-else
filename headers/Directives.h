c6e2306594b752eccdb4d0921dfdf7fe78cca94a
Nicolas Ygrand <nicolas.ygrand@gmail.com>
2013-04-13 10:35:33 +0200

    Fixed includes for Directives.h and change SOCKET arg from copy to reference.

--- headers/Directives.h
+++ headers/Directives.h
@@ -10,6 +10,9 @@
 typedef SOCKET t_socket;
 # else
 # include <netinet/in.h>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <netinet/ip.h>
 typedef	int		t_socket;
 # endif
 
@@ -17,8 +20,8 @@ class Directives
 {
 public:
 	virtual ~Directives() {};
-	virtual	void	callDirective(DirectivesOrder directiveorder, Request & request, SOCKET socket, sockaddr_in connexionInfos) = 0;
-	virtual	void	callDirective(DirectivesOrder directiveorder, Response & response, SOCKET socket, sockaddr_in connexionInfos) = 0;
+	virtual	void	callDirective(DirectivesOrder directiveorder, Request & request, SOCKET socket, struct sockaddr_in & connexionInfos) = 0;
+	virtual	void	callDirective(DirectivesOrder directiveorder, Response & response, SOCKET socket, struct sockaddr_in & connexionInfos) = 0;
 	virtual	void	init() = 0;
 };
 
