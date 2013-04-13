cb9b59c4e0789ddd4cf0da044eefb747839029ec
Nicolas Ygrand <nicolas.ygrand@gmail.com>
2013-04-13 10:44:05 +0200

    Recommit...

---
+++ headers/Directives.h
@@ -0,0 +1,29 @@
+#ifndef __DIRECTIVES_H__
+# define	__DIRECTIVES_H__
+
+#include "Request.h"
+#include "Response.h"
+#include "DirectivesOrder.h"
+
+# ifdef _WIN32
+# include	<WinSock2.h>
+typedef SOCKET t_socket;
+# else
+# include <netinet/in.h>
+# include <sys/socket.h>
+# include <netinet/in.h>
+# include <netinet/ip.h>
+typedef	int		t_socket;
+# endif
+
+class Directives
+{
+public:
+	virtual ~Directives() {};
+	virtual	void	callDirective(DirectivesOrder directiveorder, Request & request, SOCKET socket, struct sockaddr_in & connexionInfos) = 0;
+	virtual	void	callDirective(DirectivesOrder directiveorder, Response & response, SOCKET socket, struct sockaddr_in & connexionInfos) = 0;
+	virtual	void	init() = 0;
+};
+
+
+#endif // !_DIRECTIVES_H_
